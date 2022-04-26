2b32c77a0cba5b6ffc78c858de8ade1c53e1dab9
Selçuk Can <selcuk@Selcuk-MacBook-Air.local>
2022-04-26 20:50:48 +0300

    changed to make it work other way around too

--- examples/custom-event-emit-and-listen/index.js
+++ examples/custom-event-emit-and-listen/index.js
@@ -13,20 +13,31 @@ const server = createServer(app);
 const wss = new WebSocketServer({ server });
 
 wss.on('connection', function (ws) {
-  ws.on('close', function () {
-    console.log('stopping client interval');
-  });
-
   // make a dispatcher for message event which is the entry point of all events emitted from the client then register your event handlers
   ws.on("message", function (data) {
     const parsed = JSON.parse(data);
     const { eventName, payload } = parsed;
     ws.emit(eventName, payload);
   });
-
+  
   // register your event handlers
   ws.on("someEventName", function (data) {
     console.log("someEventName event happened. data: ", data);
+    ws.send(JSON.stringify({eventName: 'someEvent', payload: data}));
+  });
+  
+  const id = setInterval(function () {
+    ws.send(JSON.stringify({eventName: 'memoryUsage', payload: process.memoryUsage()}), function () {
+      //
+      // Ignore errors.
+      //
+    });
+  }, 100);
+  console.log('started client interval');
+
+  ws.on('close', function () {
+    console.log('stopping client interval');
+    clearInterval(id);
   });
 });
 
